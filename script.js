f11246b86b9bdb22d72fb2898d1d154e9039aa94
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-11-05 12:45:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 function threeSum(arr, target) {
 // write your code here
+	function threeSum(nums, target) {
+    // Sort the input array
+    nums.sort((a, b) => a - b);
+
+    let closestSum = Number.MAX_VALUE;
+    let closestDiff = Number.MAX_VALUE;
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+            const diff = Math.abs(sum - target);
+
+            if (diff < closestDiff) {
+                closestDiff = diff;
+                closestSum = sum;
+            }
+
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return closestSum;
+}
+
+// Example usage:
+const nums = [-1, 2, 1, -4];
+const target = 1;
+const result = threeSum(nums, target);
+console.log(result); // Output: 2
+
   
 }
 
